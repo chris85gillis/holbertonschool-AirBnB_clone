56a72285fb06214bd8bb72380a5586a2c923b1dd
chris85gillis <chris85gillis@gmail.com>
2023-10-06 15:02:43 -0700

    added test files to get us started

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
---
+++ tests/test_models/test_place.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines unittests for models/place.py.
+
+Unittest classes:
+    TestPlace_instantiation
+    TestPlace_save
+    TestPlace_to_dict
+"""
