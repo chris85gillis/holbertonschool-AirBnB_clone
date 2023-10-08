8618b56c3ddbd12b941ab78b0f3626edb17eb0c7
chris85gillis <chris85gillis@gmail.com>
2023-10-08 14:12:44 -0700

    added base_model code and testing

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+    
+    def __init__(self):
+        """Initialize a new BaseModel."""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.update_at = datetime.today()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,52 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
+"""Defines unittests for models/base_model.py."""
 
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
-"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the BaseModel class"""
+
+    def test_attributes(self):
+        """Test if attributes are correctly assigned."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+
+        self.assertEqual(my_model.name, "My First Model")
+        self.assertEqual(my_model.my_number, 89)
+
+    def test_str_method(self):
+        """Test the __str__ method of BaseModel."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+
+        expected_str = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+
+        self.assertEqual(str(my_model), expected_str)
+
+    def test_save_method(self):
+        """Test the save method of BaseModel."""
+        my_model = BaseModel()
+        initial_updated_at = my_model.updated_at
+
+        my_model.save()
+        updated_updated_at = my_model.updated_at
+
+        self.assertNotEqual(initial_updated_at, updated_updated_at)
+
+    def test_to_dict_method(self):
+        """Test the to_dict method of BaseModel."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+
+        my_model_dict = my_model.to_dict()
+
+        self.assertEqual(my_model_dict['__class__'], "BaseModel")
+        self.assertEqual(my_model_dict['name'], "My First Model")
+        self.assertEqual(my_model_dict['my_number'], 89)
+
+if __name__ == "__main__":
+    unittest.main()
