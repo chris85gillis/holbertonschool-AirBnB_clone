37483baf213fcf35fe91a5647645056d097e4881
abrielleperry <abrielleperry22@icloud.com>
2023-10-09 14:39:49 -0700

    typo fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
         for key, value in self.__objects.items():
             data[key] = value.to_dict()
             with open("self.__objects", "w", encoding="utf-8") as file:
-                json.dump(data, self.__filepath)
+                json.dump(data, self.__file_path)
 
     def all(self):
         """way to access all loaded and reloaded in filestorage instance
@@ -34,20 +34,20 @@ class FileStorage:
         Args:
             obj (_type_): _description_
         """
-        named_class = obj.__class__.__FileStorage__
+        named_class = obj.__class__.__name__
         object_id = obj.id
-        key = "{}, {}".format(FileStorage, object_id)
+        key = "{}, {}".format(named_class, object_id)
         self.__objects[key] = obj
 
     def reload(self):
         """reload: deserilizes data back to json file
         """
-        __file_path = "__objects"
+        self__file_path
         if os.path.exists(__file_path):
             with open(self.__file_path, "r", encoding="utf-8") as file:
                 data = json.load(file)
                 for key, value in data.items():
-                    named_class, obj.id = key.split
+                    named_class, obj.id = key.split(", ")
                     inst_class = eval(named_class)
                     obj = inst_class(**value)
                 self.__objects[key] = obj
